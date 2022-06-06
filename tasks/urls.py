36118b211d6d809c3b86d3368193204e8b1d6ed5
warlock <h.kwiatkowski@hotmail.com>
2022-06-06 22:16:46 +0200

    task update

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from
+from . import views
 
 urlpatterns = [
-    path('', )
+    path('', views.index, name="list"),
+    path('update_task/<str:pk>/', views.updateTask, name="update_task"),
 ]
